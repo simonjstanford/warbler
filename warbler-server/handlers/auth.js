33909847b6924ccc17bcecd5d9cdaba43fb2d407
Simon Stanford <simonjstanford@gmail.com>
2020-08-29 13:19:21 +0100

    Added signin

--- warbler-server/handlers/auth.js
+++ warbler-server/handlers/auth.js
@@ -2,7 +2,30 @@ const db = require("../models");
 const jwt = require("jsonwebtoken");
 
 exports.signin = async function(req, res, next) {
-
+    try {
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
+        let {id, username, profileImageUrl} = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        if (isMatch) {
+            let token = jwt.sign({
+                id,
+                username,
+                profileImageUrl
+            }, process.env.SECRET_KEY);
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return InvalidEmailPassword(next);
+        }   
+    } catch (error) {
+        return InvalidEmailPassword(next);
+    }
 }
 
 exports.signup = async function(req, res, next) {
@@ -37,4 +60,11 @@ exports.signup = async function(req, res, next) {
             message: error.message
         });
     }
+}
+
+function InvalidEmailPassword(next) {
+    return next({
+        status: 400,
+        message: "Invalid email/password"
+    });
 }
