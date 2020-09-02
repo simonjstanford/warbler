626f655371a1a48a358d76290d9118ab989470c7
Simon Stanford <simonjstanford@gmail.com>
2020-09-02 20:22:04 +0100

    Start of client

---
+++ warbler-client/src/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Provider} from "react-redux";
+import {configureStore} from "../store";
+import {BrowserRouter as Router} from "react-router-dom";
+
+const store = configureStore();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div>Hello World!</div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
