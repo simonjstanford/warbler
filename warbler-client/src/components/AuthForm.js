dfb099d819c3a5ab37a9cab2769efde7e6180541
Simon Stanford <simonjstanford@gmail.com>
2020-09-06 20:45:36 +0100

    Work on auth

--- warbler-client/src/components/AuthForm.js
+++ warbler-client/src/components/AuthForm.js
@@ -11,9 +11,23 @@ export default class AuthForm extends Component {
         }
     }
 
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const authType = this.props.signUp ? "signup" : "signin";
+        this.props.onAuth(authType, this.state).then(() => {
+            console.log("LOGGED IN");
+        });
+    }
+
     render() {
         const {email, username, password, profileImageUrl} = this.state;
-        const {heading, buttonText} = this.props;
+        const {heading, buttonText, signUp} = this.props;
         return (
             <div>
                 <div className="row justify-content-md-centre text-centre">
@@ -21,9 +35,20 @@ export default class AuthForm extends Component {
                         <form onSubmit={this.handleSubmit}>
                             <h2>{heading}</h2>
                             <label htmlFor="email">Email:</label>
-                            <input className="form-control" id="email" name="email" onChange={this.handleSubmit} value={email} type="email"/>
+                            <input className="form-control" id="email" name="email" onChange={this.handleChange} value={email} type="email"/>
                             <label htmlFor="password">Password:</label>
-                            <input className="form-control" id="password" name="password" onChange={this.handleSubmit} value={password} type="password"/>
+                            <input className="form-control" id="password" name="password" onChange={this.handleChange} value={password} type="password"/>
+                            {signUp && (
+                                <div>      
+                                    <label htmlFor="username">Username:</label>
+                                    <input className="form-control" id="username" name="username" onChange={this.handleChange} value={username} type="text"/>
+                                    <label htmlFor="image-url">Image URL:</label>
+                                    <input className="form-control" id="image-url" name="profileImageUrl" onChange={this.handleChange} value={profileImageUrl} type="text"/>
+                                </div>  
+                            )}
+                            <button className="btn btn-primary btn-block btn-lg" type="submit">
+                                {buttonText}
+                            </button>
                         </form>
                     </div>   
                 </div>
--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,21 +2,23 @@ import React from "react";
 import {Switch, Route, withRouter, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import Homepage from "../components/Homepage";
-import AuthForm from "../components/AuthForm"
+import AuthForm from "../components/AuthForm";
+import {authUser} from "../store/actions/auth";
 
 const Main = props => {
+    const {authUser} = props;
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props}/>}/>
                 <Route exact path="/signin" render={props => {
                     return (
-                        <AuthForm buttonText="Log in" heading="Welcome Back." {...props}/>
+                        <AuthForm onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props}/>
                     )
                 }}/>
                 <Route exact path="/signup" render={props => {
                     return (
-                        <AuthForm buttonText="Sign me up!" heading="Join Warbler today." {...props}/>
+                        <AuthForm onAuth={authUser} signUp buttonText="Sign me up!" heading="Join Warbler today." {...props}/>
                     )
                 }}/>
             </Switch>
@@ -30,4 +32,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, {authUser})(Main));
