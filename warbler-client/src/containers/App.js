48ddc300fba73eea938f9731cb5926a4465db1d6
Simon Stanford <simonjstanford@gmail.com>
2020-09-06 13:40:11 +0100

    Added structure, styling and images

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Main from "./Main";
 
 const store = configureStore();
 
@@ -9,7 +11,10 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div>Hello World!</div>
+        <div className="onboarding">
+          <Navbar/>
+          <Main/>
+        </div>
       </Router>
     </Provider>
   );
