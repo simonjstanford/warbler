e4cf9324896f9c478d00939016070944fae907e5
Simon Stanford <simonjstanford@gmail.com>
2020-09-07 21:09:13 +0100

    Added error handling

--- warbler-client/src/components/AuthForm.js
+++ warbler-client/src/components/AuthForm.js
@@ -27,13 +27,21 @@ export default class AuthForm extends Component {
 
     render() {
         const {email, username, password, profileImageUrl} = this.state;
-        const {heading, buttonText, signUp} = this.props;
+        const {heading, buttonText, signUp, errors, history, removeError} = this.props;
+
+        history.listen(() => { 
+            removeError();
+        });
+
         return (
             <div>
                 <div className="row justify-content-md-centre text-centre">
                     <div className="col-md-6">
                         <form onSubmit={this.handleSubmit}>
                             <h2>{heading}</h2>
+                            {errors.message && (
+                                <div className="alert alert-danger">{errors.message}</div>
+                            )}
                             <label htmlFor="email">Email:</label>
                             <input className="form-control" id="email" name="email" onChange={this.handleChange} value={email} type="email"/>
                             <label htmlFor="password">Password:</label>
