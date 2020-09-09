e3ced3a13e01383d3af1f2c2ff6172d4cb746624
Simon Stanford <Simon.Stanford@domino-uk.com>
2020-09-09 21:13:29 +0100

    Added logout

--- warbler-client/src/store/actions/auth.js
+++ warbler-client/src/store/actions/auth.js
@@ -26,3 +26,10 @@ export function authUser(type, userData) {
         });
     }
 }
+
+export function logout() {
+    return dispatch => {
+        localStorage.clear();
+        dispatch(setCurrentUser({}));
+    }
+}
